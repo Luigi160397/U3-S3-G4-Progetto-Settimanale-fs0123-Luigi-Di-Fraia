d802786a83118e6e1c4990f738a3ccbf0893242e
Luigi160397 <luigi.difraia.97@gmail.com>
2023-04-15 13:38:48 +0200

    fav album artist

--- src/components/Canzone.jsx
+++ src/components/Canzone.jsx
@@ -1,10 +1,13 @@
-import { Col, Image } from "react-bootstrap";
+import { Button, Col, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { getSelectedSong } from "../redux/actions";
-import { useDispatch } from "react-redux";
+import { getFavouriteAction, getSelectedSong } from "../redux/actions";
+import { useDispatch, useSelector } from "react-redux";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 
 const Canzone = ({ song }) => {
   const dispatch = useDispatch();
+  const favourite = useSelector(state => state.home.favourite);
+  const isFavourite = favourite.some(favSong => favSong.id === song.id);
   return (
     <Col className="col-sm-auto col-md-auto text-center mb-5">
       <a href="#i" onClick={() => dispatch(getSelectedSong(song))}>
@@ -17,6 +20,14 @@ const Canzone = ({ song }) => {
         <Link className="text-decoration-none" to={`/album/${song.album.id}`}>
           Album: {song.album.title}
         </Link>
+        <Button
+          onClick={() => dispatch(getFavouriteAction(song))}
+          size="sm"
+          variant="outline-secondary"
+          className="px-2 py-1 pb-2"
+        >
+          {isFavourite ? <FaHeart /> : <FaRegHeart />}
+        </Button>
       </p>
     </Col>
   );
